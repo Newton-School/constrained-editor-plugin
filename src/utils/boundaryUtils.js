a3e69bdd9ba30f3d99f8258ca1b8cc69631bc772
Nightrider0098 <hitesh08011999@gmail.com>
2023-06-01 00:37:23 +0530

    Update build

--- src/utils/boundaryUtils.js
+++ src/utils/boundaryUtils.js
@@ -10,27 +10,27 @@ export const getEditorBoundaryExcludingBoundaryList = (
       (boundary.startLineNumber === lastBoundaryDetails.startLineNumber &&
         boundary.startColumn > lastBoundaryDetails.startColumn)
     ) {
-      finalBoundaryList.push([
-        lastBoundaryDetails.startLineNumber,
-        lastBoundaryDetails.startColumn,
-        boundary.startLineNumber,
-        boundary.startColumn,
-      ]);
+      finalBoundaryList.push({
+        startLineNumber: lastBoundaryDetails.startLineNumber,
+        startColumn: lastBoundaryDetails.startColumn,
+        endLineNumber: boundary.startLineNumber,
+        endColumn: boundary.startColumn,
+      });
     }
     if (
       index === boundaryList.length - 1 &&
       (boundary.endLineNumber !== lastBoundaryDetails.endLineNumber ||
         boundary.endColumn !== lastBoundaryDetails.endColumn)
     ) {
-      finalBoundaryList.push([
-        boundary.endLineNumber,
-        boundary.endColumn,
-        lastBoundaryDetails.endLineNumber,
-        lastBoundaryDetails.endColumn,
-      ]);
+      finalBoundaryList.push({
+        startLineNumber: boundary.endLineNumber,
+        startColumn: boundary.endColumn,
+        endLineNumber: lastBoundaryDetails.endLineNumber,
+        endColumn: lastBoundaryDetails.endColumn,
+      });
     }
     lastBoundaryDetails.startLineNumber = boundary.endLineNumber;
-    lastBoundaryDetails.startColumn = boundary.endColumn    ;
+    lastBoundaryDetails.startColumn = boundary.endColumn;
   });
   return finalBoundaryList;
 };
